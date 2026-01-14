f58f89c763aef468794821e7b9bd6ffba01bc55e
정용철\jyc <jyc6863972@naver.com>
2026-01-14 17:39:22 +0900

    소스트리 첫

---
+++ 1.LGDX_python_study/Module.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add_num(num1,num2):
+    return num1+num2
+
+
+# In[2]:
+
+
+def sub_num(num1,num2):
+    return num1-num2
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
